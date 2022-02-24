dde16565b95c5c2c101ad3200e5b2321abc90ff4
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2022-02-25 01:08:36 +0900

    hello world

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: "development",
+
+  entry: "./src/main.ts",
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
